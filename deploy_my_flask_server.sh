377abb9e6da09905dd72cd62037c71241a92bcb4
yuanshuai <1037930135@qq.com>
2022-01-20 18:20:35 +0800

    modify words

--- deploy_my_flask_server.sh
+++ deploy_my_flask_server.sh
@@ -4,10 +4,10 @@ mkdir vir_env
 tar -zxf new.tar.gz -C vir_env
 source vir_env/bin/activate
 echo 'finish to set virtual environment'
-echo 'start to prepara sqlite3 database and add test data'
+echo 'start to prepare sqlite3 database and add test data'
 touch ceshi.db
 python add_db_data.py
-echo 'finish to prepara sqlite3 database and add test data'
+echo 'finish to prepare sqlite3 database and add test data'
 echo 'start the server'
 export FLASK_APP=app_run.py
 flask run --port 40000
